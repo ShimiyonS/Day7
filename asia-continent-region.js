c95c0294ae47fdec93942846c1fd4f899d862096
ShimiyonS <112162811+ShimiyonS@users.noreply.github.com>
2022-11-07 16:44:42 +0530

    Add files via upload

---
+++ asia-continent-region.js
@@ -0,0 +1,53 @@
+//Get all the countries from Asia continent /region using Filter function
+
+var request=new XMLHttpRequest();
+request.open('GET','https://restcountries.com/v3.1/all','true');
+request.send();
+request.onload=function (){
+    var countryData=JSON.parse(this.response);
+    const asia=countryData.filter((element)=>{
+        if(element.region==='Asia'){
+            return element.name;
+        }
+    })
+    console.log(asia);}
+//Get all the countries with population of less than 2 lacs using Filter function
+
+var request=new XMLHttpRequest();
+request.open('GET','https://restcountries.com/v3.1/all','true');
+request.send();
+request.onload=function (){
+    var countryData=JSON.parse(this.response);
+    const popu=countryData.filter((element)=>{
+        return element.population<200000;
+    })
+    console.log(popu);}
+
+//Print the following details name, capital, flag using forEach function.
+
+var request=new XMLHttpRequest();
+request.open('GET','https://restcountries.com/v3.1/all','true');
+request.send();
+request.onload=function (){
+    var countryData=JSON.parse(this.response);
+    countryData.forEach((element)=>{
+        console.log(`
+        NAME is ${element.name.common},
+        CAPITAL ${element.capital},
+        FLAG ${element.flag}`);
+    })}
+
+//Print the total population of countries using reduce function 
+
+
+var request=new XMLHttpRequest();
+request.open('GET','https://restcountries.com/v3.1/all','true');
+request.send();
+request.onload=function (){
+    var countryData=JSON.parse(this.response);
+    const population=countryData.reduce((acc,element)=>{
+        return acc+element.population;
+    },0)
+    console.log(population);}
+
+
